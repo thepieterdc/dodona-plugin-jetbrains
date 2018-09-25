17bfc9c4ce68e23b8ea40a7999b52804ebae4594
Pieter De Clercq <pieterdeclercq@outlook.com>
2018-09-25 11:55:24 +0200

    Build fix'd

--- src/main/java/be/ugent/piedcler/dodona/plugin/StringUtils.java
+++ src/main/java/be/ugent/piedcler/dodona/plugin/StringUtils.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2018. All rights reserved.
+ *
+ * @author Pieter De Clercq
+ * @author Tobiah Lissens
+ *
+ * https://github.com/thepieterdc/ugent-dodona/
+ */
 package be.ugent.piedcler.dodona.plugin;
 
 import java.util.regex.Matcher;
