c8d9d13368b328dab3531184e98c95499ea85ca5
Pieter De Clercq <pieterdeclercq@outlook.com>
2019-03-02 21:54:41 +0100

    Hide correct exercises checkbox

--- src/main/java/be/ugent/piedcler/dodona/plugin/ui/ExerciseSelectionDialog.java
+++ src/main/java/be/ugent/piedcler/dodona/plugin/ui/ExerciseSelectionDialog.java
@@ -8,6 +8,7 @@
  */
 package be.ugent.piedcler.dodona.plugin.ui;
 
+import be.ugent.piedcler.dodona.plugin.settings.DodonaSettings;
 import be.ugent.piedcler.dodona.plugin.ui.listeners.SelectedItemListener;
 import be.ugent.piedcler.dodona.resources.Exercise;
 import com.intellij.ui.CollectionListModel;
@@ -17,7 +18,10 @@ import org.jetbrains.annotations.Nullable;
 
 import javax.annotation.Nonnull;
 import javax.swing.*;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * A dialog that allows the user to select an exercise.
@@ -28,7 +32,7 @@ public class ExerciseSelectionDialog extends SelectionDialog<Exercise> {
 	private JBList<Exercise> exercisesList;
 	private final CollectionListModel<Exercise> exercisesModel;
 	
-	private JCheckBox hideCompletedCheck;
+	private JCheckBox hideCorrectCheck;
 	
 	private final SimpleObjectProperty<Exercise> selectedExercise;
 	
@@ -37,17 +41,31 @@ public class ExerciseSelectionDialog extends SelectionDialog<Exercise> {
 	 *
 	 * @param exercises the exercises to select from
 	 */
-	public ExerciseSelectionDialog(final Collection<Exercise> exercises) {
+	public ExerciseSelectionDialog(@Nonnull final Collection<Exercise> exercises) {
 		this.exercisesModel = new CollectionListModel<>(exercises);
 		this.selectedExercise = new SimpleObjectProperty<>(null);
 		
-		this.createComponents();
+		this.setContentPane(this.contentPane);
+		this.setModal(true);
+		
+		final DodonaSettings settings = DodonaSettings.getInstance();
 		
 		this.exercisesList.addListSelectionListener(e -> this.selectedExercise.set(this.exercisesList.getSelectedValue()));
 		this.exercisesList.setCellRenderer(new ExerciseListRenderer());
 		this.exercisesList.setEmptyText("No exercises were found in this series.");
 		this.exercisesList.setModel(this.exercisesModel);
 		this.exercisesList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		
+		this.hideCorrectCheck.setEnabled(!exercises.isEmpty());
+		this.hideCorrectCheck.addItemListener(e -> {
+			final boolean hide = this.hideCorrectCheck.isSelected();
+			settings.setHideCorrectExercises(hide);
+			final Exercise selected = this.selectedExercise.get();
+			this.exercisesModel.replaceAll(new ArrayList<>(hide ? getIncorrectExercises(exercises) : exercises));
+			this.exercisesList.setSelectedValue(selected, true);
+		});
+		
+		this.hideCorrectCheck.setSelected(settings.hideCorrectExercises());
 	}
 	
 	@Override
@@ -56,11 +74,16 @@ public class ExerciseSelectionDialog extends SelectionDialog<Exercise> {
 	}
 	
 	/**
-	 * Creates the form components.
+	 * Gets all incorrect exercises.
+	 *
+	 * @param all all exercises
+	 * @return all incorrect exercises
 	 */
-	private void createComponents() {
-		this.setContentPane(this.contentPane);
-		this.setModal(true);
+	@Nonnull
+	private static List<Exercise> getIncorrectExercises(@Nonnull final Collection<Exercise> all) {
+		return all.stream()
+			.filter(e -> !(e.isLastSolutionCorrect() && e.hasCorrectSolution()))
+			.collect(Collectors.toList());
 	}
 	
 	@Nullable
