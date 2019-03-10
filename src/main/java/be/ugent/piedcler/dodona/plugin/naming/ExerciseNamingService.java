05709f2f0095c9e513c21ac65ee7c349476f6c79
Pieter De Clercq <pieterdeclercq@outlook.com>
2019-03-10 02:14:30 +0100

    Finished naming implementation

--- src/main/java/be/ugent/piedcler/dodona/plugin/naming/ExerciseNamingService.java
+++ src/main/java/be/ugent/piedcler/dodona/plugin/naming/ExerciseNamingService.java
@@ -11,6 +11,7 @@ package be.ugent.piedcler.dodona.plugin.naming;
 import be.ugent.piedcler.dodona.resources.Exercise;
 
 import javax.annotation.Nonnull;
+import java.util.Optional;
 
 /**
  * A naming service for exercises.
@@ -21,8 +22,8 @@ public interface ExerciseNamingService {
 	 * Generates a filename for the given resource.
 	 *
 	 * @param exercise the exercise to name
-	 * @return the generated filename
+	 * @return the generated filename, null if failed
 	 */
 	@Nonnull
-	String generateFileName(@Nonnull Exercise exercise);
+	Optional<String> generateFileName(@Nonnull Exercise exercise);
 }
