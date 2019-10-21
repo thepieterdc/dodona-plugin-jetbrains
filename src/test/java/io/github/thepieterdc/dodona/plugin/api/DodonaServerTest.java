4a2b0cacfb7ef369a11f07224689245bd736e7a9
Pieter De Clercq <pieterdeclercq@outlook.com>
2019-10-21 09:49:46 +0200

    Implement coverage tracking

---
+++ src/test/java/io/github/thepieterdc/dodona/plugin/api/DodonaServerTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2018-2019. All rights reserved.
+ *
+ * @author Pieter De Clercq
+ * @author Tobiah Lissens
+ *
+ * https://github.com/thepieterdc/dodona-plugin-jetbrains/
+ */
+package io.github.thepieterdc.dodona.plugin.api;
+
+import org.jetbrains.annotations.NonNls;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Tests DodonaServer.
+ */
+public class DodonaServerTest {
+	@NonNls
+	private static final String URL = "https://some.dodona.instance/";
+	
+	/**
+	 * Tests .toDisplayName() using known servers.
+	 */
+	@Test
+	public void testToDisplayNameKnown() {
+		for (final DodonaServer server : DodonaServer.values()) {
+			Assert.assertEquals(
+				server.getDisplayName(),
+				DodonaServer.toDisplayName(server.getUrl())
+			);
+		}
+	}
+	
+	/**
+	 * Tests .toDisplayName() using an unknown server.
+	 */
+	@Test
+	public void testToDisplayNameUnknown() {
+		Assert.assertEquals(
+			DodonaServerTest.URL,
+			DodonaServer.toDisplayName(DodonaServerTest.URL)
+		);
+	}
+	
+	/**
+	 * Tests .toIcon() using known servers.
+	 */
+	@Test
+	public void testToIconKnown() {
+		for (final DodonaServer server : DodonaServer.values()) {
+			Assert.assertEquals(
+				server.getIcon(),
+				DodonaServer.toIcon(server.getUrl())
+			);
+		}
+	}
+	
+	/**
+	 * Tests .toIcon() using an unknown server.
+	 */
+	@Test
+	public void testToIconUnknown() {
+		Assert.assertEquals(
+			DodonaServer.LOCAL3000.getIcon(),
+			DodonaServer.toIcon(DodonaServerTest.URL)
+		);
+	}
+}
