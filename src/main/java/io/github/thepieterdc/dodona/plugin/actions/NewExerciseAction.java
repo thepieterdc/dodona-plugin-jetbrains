98fbb7f6e7d9530f9a37d0f02b4f21d7889bf9c1
Pieter De Clercq <pieterdeclercq@outlook.com>
2019-10-21 18:36:23 +0200

    Add empty action

---
+++ src/main/java/io/github/thepieterdc/dodona/plugin/actions/NewExerciseAction.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2018-2019. All rights reserved.
+ *
+ * @author Pieter De Clercq
+ * @author Tobiah Lissens
+ *
+ * https://github.com/thepieterdc/dodona-plugin-jetbrains/
+ */
+
+package io.github.thepieterdc.dodona.plugin.actions;
+
+public class NewExerciseAction {
+}
