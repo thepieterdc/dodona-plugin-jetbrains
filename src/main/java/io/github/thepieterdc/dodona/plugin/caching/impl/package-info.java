2becd68c95be60db208e2227ab186f4f8ca50c6f
Pieter De Clercq <pieterdeclercq@outlook.com>
2019-10-23 00:03:11 +0200

    Create simple cache

---
+++ src/main/java/io/github/thepieterdc/dodona/plugin/caching/impl/package-info.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018-2019. All rights reserved.
+ *
+ * @author Pieter De Clercq
+ * @author Tobiah Lissens
+ *
+ * https://github.com/thepieterdc/dodona-plugin-jetbrains/
+ */
+
+/**
+ * Implementations of a CachingService.
+ */
+@ParametersAreNonnullByDefault
+package io.github.thepieterdc.dodona.plugin.caching.impl;
+
+import javax.annotation.ParametersAreNonnullByDefault;
