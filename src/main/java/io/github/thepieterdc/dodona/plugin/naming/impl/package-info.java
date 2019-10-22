424ac1158c80aaa963c00bc4ecc56164e5d77a19
Pieter De Clercq <pieterdeclercq@outlook.com>
2019-10-23 00:22:13 +0200

    Add naming service

---
+++ src/main/java/io/github/thepieterdc/dodona/plugin/naming/impl/package-info.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018-2019. All rights reserved.
+ *
+ * @author Pieter De Clercq
+ * @author Tobiah Lissens
+ *
+ * https://github.com/thepieterdc/dodona-plugin-jetbrains/
+ */
+
+/**
+ * Implementations of naming services.
+ */
+@ParametersAreNonnullByDefault
+package io.github.thepieterdc.dodona.plugin.naming.impl;
+
+import javax.annotation.ParametersAreNonnullByDefault;
